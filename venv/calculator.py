fb625d26080b1591ed709d289873e1b42326bc17
evgeni-gusin <evgeni.gusin@gmail.com>
2020-05-23 18:19:30 +0300

    fll coment

--- venv/calculator.py
+++ venv/calculator.py
@@ -7,14 +7,15 @@ def calc_prob(fascist_cards, liberal_cards):
     # FFF prob
     fff_prob = fascist_cards/remaining_cards * (fascist_cards-1)/(remaining_cards-1) * (fascist_cards-2)/(remaining_cards-2)
     print('FFF -> ' + str(round(fff_prob, 2)))
-    # FFL prob
-
-    # FLL prob
-
-    # LLL prob
+    #LLL prob
     lll_prob = liberal_cards/remaining_cards * (liberal_cards-1)/(remaining_cards-1) *(liberal_cards-2)/(remaining_cards-2)
     print('LLL -> ' + str(round(lll_prob,2)))
-
+    #FFL prob
+    ffl_prob = fascist_cards/remaining_cards * (fascist_cards-1)/(remaining_cards-1)* liberal_cards/(remaining_cards-2)
+    print('FFL ->' + str(round(ffl_prob,2)))
+    #FLL prob
+    fll_prob = fascist_cards/remaining_cards * (liberal_cards-1)/(remaining_cards-1) *(liberal_cards-2)/(remaining_cards-2)
+    print('FLL ->' + str(round(fll_prob,2)))
 calc_prob(11,6)
 
 
