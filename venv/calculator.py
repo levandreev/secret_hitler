fefbdac8c21d6fa7aaaccde903a0a7883b08d3ef
Lev <duffyduck451@gmail.com>
2020-05-23 16:54:22 +0200

    fff prob added

--- venv/calculator.py
+++ venv/calculator.py
@@ -1,1 +1,16 @@
-print("dsvdfi")
+L = 6
+F = 11
+
+11/17 * 10/16 * 9/15
+
+def calc_prob(fascist_cards, liberal_cards):
+    # FFF prob
+    fff_prob = fascist_cards/(fascist_cards+liberal_cards) * (fascist_cards-1)/(fascist_cards+liberal_cards-1) * (fascist_cards-2)/(fascist_cards+liberal_cards-2)
+    print('FFF -> ' + str(round(fff_prob, 2)))
+    # FFL prob
+
+    # FLL prob
+
+    # LLL prob
+
+calc_prob(11,6)
