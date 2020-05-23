9f1e0ac89200f4f1ffb983af3cc2af1d03924b5f
Lev <duffyduck451@gmail.com>
2020-05-23 18:49:35 +0200

    claims added

--- venv/calculator.py
+++ venv/calculator.py
@@ -1,9 +1,8 @@
 L = 6
 F = 11
-
-
-
-def calc_prob(fascist_cards, liberal_cards):
+def calc_prob():
+    fascist_cards = F
+    liberal_cards = L
     remaining_cards = fascist_cards+liberal_cards
     # FFF prob
     fff_prob = fascist_cards/remaining_cards * (fascist_cards-1)/(remaining_cards-1) * (fascist_cards-2)/(remaining_cards-2)
@@ -31,6 +30,27 @@ def calc_prob(fascist_cards, liberal_cards):
     # sum for FLL
     fll_real = fll_prob + lfl_prob + llf_prob
     print('FLL -> ' + str(round(fll_real,2)))
-calc_prob(11,6)
 
+calc_prob()
 
+def claims_fff():
+    global F
+    F = F-3
+    calc_prob()
+
+def claims_ffl():
+    global F
+    global L
+    F = F-2
+    L = L-1
+    calc_prob()
+def claims_fll():
+    global F
+    global L
+    F = F-1
+    L = L-2
+    calc_prob()
+def claims_lll():
+    global L
+    L = L-3
+    calc_prob()
